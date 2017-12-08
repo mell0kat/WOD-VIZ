45887ea342bff25e1d3cc675ac93531f09228774
Katherine Mello <katherine.mello8@gmail.com>
2017-12-08 12:26:40 -0500

    Converts SingleExercise to styled components

--- src/components/SingleExercise.tsx
+++ src/components/SingleExercise.tsx
@@ -1,20 +1,61 @@
 import * as React from 'react'
 import * as Moment from 'moment'
 import { connectToSpreadsheet } from "react-google-sheet-connector"
+import styled from 'styled-components'
 
-const xWidth = 450
-const yHeight = 250
+const X_AXIS = 450
+const Y_AXIS = 250
 
-const computeBarStyle = (color) => ({
-	backgroundColor: color,
-})
+const BarGraph = styled.div`
+	width: ${X_AXIS}px;
+	height: ${Y_AXIS}px;
+	border-left: white 2px solid;
+	border-bottom: white 2px solid;
+	align-self: center;
+	display: flex;
+	align-items: flex-end;
+	position: relative;
+	padding-right: ${props => (props.smallestDx / props.dateRange * X_AXIS)}px
+`
 
-const getTitleColor = (color) => ({ color })
+const Title = styled.p`
+	font-size: 24px;
+	font-family: BaseFont;
+	color: ${props => props.color};
+`
 
+const Label = styled.text`
+	font-size: 8px;
+	text-align: center;
+	color: white;
+`
+const Bar = styled.div`
+	display: inline-block;
+	position: absolute;
+	background-color: ${props => props.exerciseColor};
+	height: ${props => getBarHeight(props.datum, props.rows, props.yAxis)}px;
+`
+
+const BarGraphContainer = styled.div`
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+const MarkerContainer = styled.div`
+	width: ${X_AXIS}px;
+	position: relative;
+`
+
+const Marker = styled.text`
+	color: white;
+	fontsize: 12;
+	display: inline-block;
+	position: absolute;
+`
 const getBarXPosition = (datum, dateRange, minDate, _x, smallestDx, idx, rotate?) => {
 	const dx: number = ((Moment(datum.date) - Moment(minDate)) / dateRange) * _x
 	const width: number = (smallestDx / dateRange) * _x *.75
-	console.log('width', width)
 	return {
 		left: dx,
 		transform: `${(rotate ? 'rotate(90deg)' : '')}`,
@@ -59,9 +100,7 @@ const getBarHeight = (datum: IExerciseRow, data, _y) => {
 
 	const dy = ((datum['weight (lbs)'] - minWeight + (0.5 * padding)) / (maxWeight - minWeight + padding)) * _y
 
-	return {
-		height: dy,
-	}
+	return dy
 }
 
 interface IProps {
@@ -77,27 +116,33 @@ const SingleExercise = (props: IProps) => {
 	const smallestDx = smallestDiff(exerciseData.data)
 	console.log('smallestDx', exerciseData.data)
 	return (
-		<div style={styles.container}>
-			<p style={{ ...styles.title, ...getTitleColor(exerciseColor) }}>{exerciseData.name}</p>
+		<BarGraphContainer>
+			<Title color={exerciseColor}>{exerciseData.name}</Title>
 			<p style={{ padding: 0, margin: 0, color: 'white'}}>{JSON.stringify(exerciseData.data)}</p>
-			<div style={{...styles.barGraph, paddingRight: (smallestDx / dateRange * xWidth)}}>
+			<BarGraph smallestDx={smallestDx} dateRange={dateRange}>
 				{
 					exerciseData.data.map((datum, idx) => (
-						<div
+						<Bar
 							key={`${datum.date}${datum.reps}`}
-							style={{...styles.bar, ...computeBarStyle(exerciseColor), ...getBarXPosition(datum, dateRange, exerciseData.data[0][0], xWidth, smallestDx, idx), ...getBarHeight(datum, exerciseData.data, yHeight)}}
+							exerciseColor={exerciseColor}
+							datum={datum}
+							rows={exerciseData.data}
+							yAxis={Y_AXIS}
+							dateRange={dateRange}
+							smallestDx={smallestDx}
+							style={{...getBarXPosition(datum, dateRange, exerciseData.data[0][0], X_AXIS, smallestDx, idx)}}
 						>
-						<p style={styles.label}>{datum['weight (lbs)']}</p>
-						</div>
+						<Label>{datum['weight (lbs)']}</Label>
+						</Bar>
 					))
 				}
-			</div>
-			<div style={{width: xWidth, position:'relative'}}>
+			</BarGraph>
+			<MarkerContainer>
 				{
 					monthMarkers(exerciseData.data, dateRange, smallestDx)
 				}
-			</div>
-		</div>
+			</MarkerContainer>
+		</BarGraphContainer>
 	)
 }
 
@@ -115,10 +160,13 @@ const monthMarkers = (rows: IExerciseRow[], dateRange, smallestDx) => {
 	let markerPoint = Moment(rows[0].date).startOf('month')
 	let idx = 0
 	while (markerPoint < Moment(rows[rows.length - 1].date)) {
-		markers.push(<text
-			key={`${markerPoint}`}
-			style={{...getBarXPosition({ date: markerPoint }, dateRange, rows[0][0], xWidth, smallestDx, idx), color: 'white', fontSize: 12, display: 'inline-block', position: 'absolute'}}>{Moment(markerPoint).format('MMM')}
-			</text>)
+		markers.push(
+			<Marker
+				key={`${markerPoint}`}
+				style={getBarXPosition({ date: markerPoint }, dateRange, rows[0][0], X_AXIS, smallestDx, idx)}>
+				{Moment(markerPoint).format('MMM')}
+			</Marker>
+		)
 		markerPoint = markerPoint.add(1, 'month')
 		idx++
 	}
@@ -131,7 +179,7 @@ const smallestDiff = (rows: IExerciseRow[]) => rows.reduce(
 		const nextRow = array[currentIdx + 1]
 		if (!nextRow) { return acc }
 		const diff = Math.abs(Moment(nextRow.date) - Moment(currentVal.date))
-	console.log('diff', diff, nextRow.date, currentVal.date)
+
 		if (diff < acc) {
 			return diff
 		} else {
@@ -140,48 +188,5 @@ const smallestDiff = (rows: IExerciseRow[]) => rows.reduce(
 	},
 	Infinity,
 )
-
-const styles = {
-	title: {
-		fontSize: 24,
-		fontFamily: 'BaseFont'
-	},
-	button: {
-		backgroundColor: 'transparent',
-		borderRadius: 5,
-		borderWidth: 2,
-		padding: 3,
-	},
-	barGraph: {
-		width: xWidth,
-		height: yHeight,
-		borderLeftStyle: 'solid',
-		borderBottomStyle: 'solid',
-		borderLeftWidth: 2,
-		borderBottomWidth: 2,
-		borderColor: 'white',
-		alignSelf: 'center',
-		//paddingLeft: .5 * barWidth,
-		// paddingRight: .5 * barWidth,
-		display: 'flex',
-		alignItems: 'flex-end',
-		position: 'relative',
-	},
-	bar: {
-		display: 'inline-block',
-		position: 'absolute',
-	},
-	label: {
-		fontSize: 8,
-		textAlign: 'center',
-		color: 'white',
-	},
-	container: {
-		flex: 1,
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-	},
-}
 
 export default connectToSpreadsheet(SingleExercise)
